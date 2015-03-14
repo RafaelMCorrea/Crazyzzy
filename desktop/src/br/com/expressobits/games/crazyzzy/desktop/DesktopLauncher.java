21c165c0af5735b1489e5ac3e8cc0274927f4ea8
Rafael Correa <rafinha2108@gmail.com>
2015-03-14 13:09:00 -0300

    Adição do projeto.

---
+++ desktop/src/br/com/expressobits/games/crazyzzy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.com.expressobits.games.crazyzzy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.com.expressobits.games.crazyzzy.Crazyzzy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Crazyzzy(), config);
+	}
+}
---
+++ html/src/br/com/expressobits/games/crazyzzy/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package br.com.expressobits.games.crazyzzy.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import br.com.expressobits.games.crazyzzy.Crazyzzy;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Crazyzzy();
+        }
+}
