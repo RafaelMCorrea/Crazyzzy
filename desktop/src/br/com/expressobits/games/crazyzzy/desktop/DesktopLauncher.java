93eb268d439bfa3636f152f17f218c53e9d8f929
Rafael Correa <rafinha2108@gmail.com>
2015-03-14 13:56:50 -0300

    Abstrair métodos para classe do Mário.

--- desktop/src/br/com/expressobits/games/crazyzzy/desktop/DesktopLauncher.java
+++ desktop/src/br/com/expressobits/games/crazyzzy/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import br.com.expressobits.games.crazyzzy.Crazyzzy;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 600;
+		config.height = 480;
 		new LwjglApplication(new Crazyzzy(), config);
 	}
 }
